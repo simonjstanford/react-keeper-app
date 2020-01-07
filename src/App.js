ea7a7a3f7e034e6654c51e780ff57955f2d3cc35
Simon Stanford <simonjstanford@gmail.com>
2020-01-07 19:57:59 +0000

    Create notes based on data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Note from './components/Note';
+import notes from './notes';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
+      {notes.map(x => (
+        <Note key={x.key}
+              title={x.title} 
+              content={x.content}/>))}
       <Footer />
     </div>
   );
